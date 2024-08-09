b2ab152eb2a21a0802f8825832d862db5296bcdd
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-09 15:23:43 +0200

    updated url and views

--- library Website/books/urls.py
+++ library Website/books/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BookListView
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'),
+    path('', BookListView.as_view(), name='home'),
 ]
