abbad87b71460966fbb78f3036b7f10fca1f68d2
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-11 15:16:57 +0200

    created tests

--- Todo list/todos/admin.py
+++ Todo list/todos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Todo
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'body'
+    )
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
--- Todo list/todos/tests.py
+++ Todo list/todos/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+
+#what to test?
+'''
+    we want to make sure that the title in our models is displaying texts as expected 
+'''
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up non-modified objects used by all test methods
+        cls.todo = Todo.objects.create(
+            title = "First todo",
+            body = "Go to Gymn"
+        )
+        
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "First todo")
+        self.assertEqual(self.todo.body, "Go to Gymn")
+        self.assertEqual(str(self.todo), "First todo")
+
