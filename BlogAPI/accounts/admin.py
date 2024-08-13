bf65be6a848070bb69e93184bade76969882f8b7
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-13 22:49:57 +0200

    added some bug, gotta take a bath

--- BlogAPI/accounts/admin.py
+++ BlogAPI/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        'email',
+        'username',
+        'name',
+        'is_staff'
+    ]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("name",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("name",)}),)
 
 # Register your models here.
+admin.site.register(CustomUser, CustomUserAdmin)
--- BlogAPI/accounts/forms.py
+++ BlogAPI/accounts/forms.py
@@ -4,7 +4,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + 'name'
+        fields = UserCreationForm.Meta.fields + ('name',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
