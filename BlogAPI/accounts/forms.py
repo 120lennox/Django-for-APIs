524cbb446be474f1d0a96553a72e4bd862c6b733
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-13 22:39:46 +0200

    added custom creation form and custom change form in forms.py (new file)

---
+++ BlogAPI/accounts/forms.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + 'name'
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = UserCreationForm
+        fields = UserChangeForm.Meta.fields
+
