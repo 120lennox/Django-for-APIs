7a058287400496127b63f88bac56933c5ff7ba9f
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-14 16:22:46 +0200

    added serializers and views (for endpoints logic)

--- BlogAPI/posts/views.py
+++ BlogAPI/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostListView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_classes = PostSerializer
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_classes = PostSerializer
