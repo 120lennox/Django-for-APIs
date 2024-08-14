03582d0fa7d4ed84d3741cb6a7318c81f535528a
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-14 15:55:22 +0200

    all tests passed. now delving to creating APIs for our apps

--- BlogAPI/posts/tests.py
+++ BlogAPI/posts/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
 
 # Create your tests here.
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@email.com",
+            password="secret"
+        )
+
+        cls.post = Post.objects.create(
+            author = cls.user,
+            title = "Test Post",
+            body = "Nice body"
+        )
+
+    def test_post_model(self):
+            self.assertEqual(self.post.author.username, "testuser")
+            self.assertEqual(self.post.title, "Test Post")
+            self.assertEqual(self.post.body, "Nice body")
+            self.assertEqual(str(self.post), "Test Post")
+        
