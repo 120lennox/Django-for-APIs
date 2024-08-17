701649a6743566dd68c5620426c70f95d098cb8b
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-17 12:57:48 +0200

    added a permision in "IsAuthenticated", that allows only authenticated users to have access to our API

--- BlogAPI/blog/settings.py
+++ BlogAPI/blog/settings.py
@@ -52,7 +52,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES":[
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
